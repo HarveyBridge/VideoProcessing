081980066c439382ad1b81b0e43a6f1a6634b99e
BowenHsu <BowenHsu>
2016-06-07 23:44:36 +0800

    Sobel暫時的測試

--- LocalTernaryPatterns_Imp/LocalTernaryPatterns_Imp.vhd
+++ LocalTernaryPatterns_Imp/LocalTernaryPatterns_Imp.vhd
@@ -209,7 +209,7 @@ signal LTP_Cnt					: integer range 0 to 639:=0;
 ------------------|
 --LTP Calculate --|
 ------------------|
-
+signal Sobel_Cal_en				: std_logic;
 
 begin
 
@@ -578,69 +578,6 @@ end process;
 --############################################### Buffer State ###############################################--
 
 
--- --############################################### Sobel Calculate ###############################################--
--- process(rst_system, clk_video)
--- variable sobel_x_cc_1 : std_logic_vector(9 downto 0);
--- variable sobel_x_cc_2 : std_logic_vector(9 downto 0);
--- variable sobel_y_cc_1 : std_logic_vector(9 downto 0);
--- variable sobel_y_cc_2 : std_logic_vector(9 downto 0);
--- begin
--- if rst_system = '0' then
-	-- SB_XSCR <= "0000000000";
-	-- SB_YSCR <= "0000000000";
-	-- SB_SUM  <= "00000000000";
-	-- SB_CRB_data <= '0';
--- -- system reset
-	-- redata_cnt <= 0 ;
-	-- redata_en <= '0';
--- else
-	-- if rising_edge(clk_video) then
-		-- if buf_sobel_cc_en = '1' then
-			-- if buf_data_state(0) = '1' then
-			-- -- if buf_data_state(0) = '0' then
-				-- sobel_x_cc_1 := SB_buf_0_data_1 + SB_buf_0_data_2 + SB_buf_0_data_2 + SB_buf_0_data_3;
-				-- sobel_x_cc_2 := SB_buf_2_data_1 + SB_buf_2_data_2 + SB_buf_2_data_2 + SB_buf_2_data_3;
-				
-				-- sobel_y_cc_1 := SB_buf_0_data_1 + SB_buf_1_data_1 + SB_buf_1_data_1 + SB_buf_2_data_1;
-				-- sobel_y_cc_2 := SB_buf_0_data_3 + SB_buf_1_data_3 + SB_buf_1_data_3 + SB_buf_2_data_3;
-	
-				-- if sobel_x_cc_1 >= sobel_x_cc_2 then
-					-- SB_XSCR <= sobel_x_cc_1 - sobel_x_cc_2;
-				-- else
-					-- SB_XSCR <= sobel_x_cc_2 - sobel_x_cc_1;
-				-- end if;
-				
-				-- if sobel_y_cc_1 >= sobel_y_cc_2 then
-					-- SB_YSCR <= sobel_y_cc_1 - sobel_y_cc_2;
-				-- else
-					-- SB_YSCR <= sobel_y_cc_2 - sobel_y_cc_1;
-				-- end if;
-			-- else
-				-- --if ((SB_XSCR > "0001100000" and SB_XSCR < "0011100000") or (SB_YSCR > "0001100000" and SB_YSCR < "0011100000")) then
-					-- -- SB_CRB_data <= '1';
-					
--- -- sum  X_sobel  &  Y_sobel
-				-- SB_SUM <= "00000000000"+SB_XSCR+SB_YSCR;
--- -- put SUM_sobel to SB_buf_redata(0~640)
-				-- SB_buf_redata(redata_cnt) <= SB_SUM(9 downto 2); 
--- -- counter redata_cnt to get SB_buf_redata address
-				-- if redata_cnt < 639 then
-						-- redata_cnt <= redata_cnt + 1;
-				-- else
-					-- redata_cnt <= 0;
-				-- end if;
-			-- end if;
-		-- else
-			-- SB_XSCR <= "0000000000";
-			-- SB_YSCR <= "0000000000";
-			-- SB_CRB_data <= '0';
--- -- when cnt_video_hsync > 1280, let redata_cnt be reset
-			-- redata_cnt <= 0;
-		-- end if;
-	-- end if;
--- end if;
--- end process;
--- --############################################### Sobel Calculate ###############################################--
 
 --############################################### Matrix Expression ###############################################--
 --			 Col-1   Col-2  Col-3
@@ -725,10 +662,11 @@ if rst_system = '0' then
 	SB_YSCR <= "0000000000";
 	SB_SUM  <= "00000000000";
 	redata_cnt <= 0 ;
+	Sobel_Cal_en <= '0';
 else
 	if rising_edge(clk_video) then
 		if buf_sobel_cc_en = '1' then
-			if LBP_Data_State(0) = '1' then
+			if buf_data_state(0) = '1' then
 				sobel_x_cc_1 := Matrix_R1C3 + Matrix_R2C3 + Matrix_R2C3 + Matrix_R3C3;
 				sobel_x_cc_2 := Matrix_R1C1 + Matrix_R2C1 + Matrix_R2C1 + Matrix_R3C1;
 				
@@ -746,7 +684,9 @@ else
 				else
 					SB_YSCR <= sobel_y_cc_2 - sobel_y_cc_1;
 				end if;
+				Sobel_Cal_en <= '0';
 			else
+				Sobel_Cal_en <= '1';
 -- sum  X_sobel  &  Y_sobel
 				SB_SUM <= "00000000000"+SB_XSCR+SB_YSCR;
 -- put SUM_sobel to SB_buf_redata(0~640)
@@ -764,6 +704,7 @@ else
 			SB_CRB_data <= '0';
 -- when cnt_video_hsync > 1280, let redata_cnt be reset
 			redata_cnt <= 0;
+			Sobel_Cal_en <= '0';
 		end if;
 	end if;
 end if;
